bbbeaad12139b86fe98911c1238788dcafef364e
Justyn Temme <justyntemme@gmail.com>
2018-05-17 11:29:53 -0500

    hopefully last fix for this issue

--- public/scripts.js
+++ public/scripts.js
@@ -88,8 +88,8 @@ function mapLoaded() {
 					var i = v.heatPoints.length;
 					//while (v.heatPoints.length > 0) {v.heatPoints.pop();} 
 					while (i < v.clubs[key]['count']) {
-						//Vue.set(v.heatPoints,(v.heatPoints.length + i), new google.maps.LatLng(lat, lon));
-						this.heatPoints.push(new google.maps.LatLng(lat,lon));
+						Vue.set(v.heatPoints,(v.heatPoints.length + 1), new google.maps.LatLng(lat, lon));
+						//this.heatPoints.push(new google.maps.LatLng(lat,lon));
 						i++;
 					}
 					console.log(v.heatPoints);
